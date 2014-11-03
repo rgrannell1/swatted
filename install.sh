50a9f1b77d9f4dceb75a2f661b13527400294d3e
rgrannell1 <r.grannell2@gmail.com>
2014-11-03 18:41:42 +0000

    updated installer

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ cd ~/
 rm -rf swatted
 git clone https://github.com/rgrannell1/swatted.git
 cd swatted
-echo alias swatted=~/swatted/lib/docopt-swatted.rb\' >> ~/.bashrc && . ~/.bashrc
+echo alias swatted='ruby "~/swatted/lib/docopt-swatted.rb"' >> ~/.bashrc && . ~/.bashrc
 
 cd $here
