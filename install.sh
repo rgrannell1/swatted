b8e370d1ea03acddbe361f64035af4e7c6c99c90
rgrannell1 <r.grannell2@gmail.com>
2014-11-03 18:34:09 +0000

    fixing installer

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 here=$(pwd)
+cd ~/
+
 rm -rf swatted
 git clone https://github.com/rgrannell1/swatted.git
 cd swatted
 echo alias swatted=\'$(pwd -P)/lib/docopt-swatted.rb\' >> ~/.bashrc && . ~/.bashrc
+
 cd $here
